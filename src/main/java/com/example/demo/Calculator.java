c32ce8fffedff74921a7344bcff89ac4e704832e
RafoMeliqyan <rafomelikyan10@gmail.com>
2022-08-25 23:32:13 +0400

    Add Calculator

---
+++ src/main/java/com/example/demo/Calculator.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+
+    public int sum(int x, int y) {
+        return x + y;
+    }
+
+}
